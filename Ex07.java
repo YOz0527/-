fc4fece5bef7021471bb897fe907f6a52335db5c
YOz0527 <145087325+YOz0527@users.noreply.github.com>
2023-09-22 10:27:27 +0800

    Update Ex07.java

---
+++ Ex07.java
@@ -0,0 +1,20 @@
+class CTest {
+    void test(int number) {
+        if (number == 0) {
+            System.out.println("此數為0");
+        } else if (number % 2 == 0) {
+            System.out.println("此數為偶數");
+        } else {
+            System.out.println("此數為奇數");
+        }
+    }
+
+    public static void main(String[] args) {
+        CTest tester = new CTest();
+
+        // 使用test()函数判断3、8和0各为何数。
+        tester.test(3);
+        tester.test(8);
+        tester.test(0);
+    }
+}
